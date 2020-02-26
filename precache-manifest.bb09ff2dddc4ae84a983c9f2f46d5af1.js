f173278040fdee1b421271fb2a5420ca67607d13
ghousaly <ghous.shah91@gmail.com>
2020-02-26 14:17:51 +0500

    first commit

---
+++ precache-manifest.bb09ff2dddc4ae84a983c9f2f46d5af1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a18a03f8efe5500cba360c4d13a3d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "336737c61c6245ccac5b",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "e664c1efe026d87daed5",
+    "url": "/static/css/main.fdd40446.chunk.css"
+  },
+  {
+    "revision": "336737c61c6245ccac5b",
+    "url": "/static/js/2.377d299c.chunk.js"
+  },
+  {
+    "revision": "8571ffcf81bad2aaaf59f8b1d589192d",
+    "url": "/static/js/2.377d299c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e664c1efe026d87daed5",
+    "url": "/static/js/main.60d45ddc.chunk.js"
+  },
+  {
+    "revision": "2dcec4c016f55b8d8494",
+    "url": "/static/js/runtime-main.691c549d.js"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "25fbdca0ab59275bcdea838cc1946ecc",
+    "url": "/static/media/brands.25fbdca0.png"
+  },
+  {
+    "revision": "88a6866c82c6210d711853dbaf62251a",
+    "url": "/static/media/car.88a6866c.png"
+  },
+  {
+    "revision": "763fc0b133885828385865f3b242060d",
+    "url": "/static/media/logo.763fc0b1.png"
+  },
+  {
+    "revision": "fa5f0bb016bf8cff43869a6cdf0368a5",
+    "url": "/static/media/logo1.fa5f0bb0.png"
+  },
+  {
+    "revision": "0165e8b0884355b53e9b178971ce8b4d",
+    "url": "/static/media/mobile.0165e8b0.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b3ac4053b0bbb099cb1ca64f95c2d863",
+    "url": "/static/media/slide1.b3ac4053.jpg"
+  }
+]);
